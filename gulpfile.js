825c72db4d42799ed7bb3a87d6e5d571811d9348
kaltschu <kevinaltschuler@gmail.com>
2016-05-11 15:09:54 -0400

    gulp, browsersync, less

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+
+var less = require('gulp-less');
+var path = require('path');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+ 
+gulp.task('less', function () {
+  return gulp.src('./public/**/less/**/*.less')
+  	.pipe(concat('styles.css'))
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ],
+      compress: true
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: './public'
+    },
+  })
+})
+
+gulp.task('watch', ['browserSync', 'less'],  function(){
+  gulp.watch('./public/**/less/**/*.less', ['less']); 
+})
